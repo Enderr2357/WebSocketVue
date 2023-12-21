a2a8984bf5a1ce69b46263e85ace443167719620
Enderr2357 <y2594254561@outlook.com>
2023-12-21 18:38:24 +0800

    可以发送信息到后端并被保存起来

--- src/components/showStatus.vue
+++ src/components/showStatus.vue
@@ -2,19 +2,34 @@
 import { ref } from 'vue'
 import {ElNotification,ElButton} from 'element-plus'
 const url='ws://localhost:8080/websocket'
+const info = {
+  id:1,
+  roomId:1,
+  patientId:1,
+  status:2
+}
+const equipment={
+  id:null,
+  equipmentId:"qqwe2"
+}
 const message = {
-        type: "edit",
-        data: "Hello, WebSocket!"
-};
+  type: "edit",
+  data: info
+}
+const saveMessage={
+  type:'save',
+  data: equipment
+}
 const jsonData=JSON.stringify(message);
 const socket=new WebSocket(url)
-  socket.onopen = function(e){
+socket.onopen = function(e){
   ElNotification({
     type:'success',
     message:'连接成功',
     duration :2000
-    })
-  }
+  })
+  socket.send(JSON.stringify(saveMessage))
+}
 const onclose=()=>{
   socket.close()
   ElNotification({
